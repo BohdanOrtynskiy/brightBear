75543f9302c51661ca6faa2f0d301bfcdc1fc4d2
Withcode <bohdan.ortynskiy@gmail.com>
2024-07-31 18:59:21 +0200

    Add Vue.js integration and basic component setup

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome to Our Online Store</h1>
+        <p>This is a test of the Vue.js integration.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
